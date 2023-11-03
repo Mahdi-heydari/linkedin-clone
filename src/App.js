d1aed4af79dc57931a3e952ab80f8b61625d374f
mahdi85lovex94 <mahdi.funlife@gmail.com>
2023-11-03 15:59:42 +0330

    🚪 Implement the login component  with its styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
 export default function App() {
   return (
-    <div>
-      
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
