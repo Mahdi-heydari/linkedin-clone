632cdf706bcd77bb0447c9063eadfb5f6fc7bfd6
mahdi85lovex94 <mahdi.funlife@gmail.com>
2023-11-09 23:00:21 +0330

    ⬅️ Implementing the Leftside component/section with its styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,29 @@
 import styled from "styled-components";
 import Header from "../UI/Header";
+import Leftside from "../UI/Leftside";
 
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header />
 
       <Container>
-      <Section>
-        <h5>
-          <a>Hiring in a hurry? - </a>
-        </h5>
-        <p>
-          Find talented pros in record time with Upwork and keep business
-          moving.
-        </p>
-      </Section>
-      <Layout>
-        {/* <Leftside />
-        <Main />
-        <Rightside /> */}
-      </Layout>
-    </Container>
+        <Section>
+          <h5>
+            <a>Hiring in a hurry? - </a>
+          </h5>
+          <p>
+            Find talented pros in record time with Upwork and keep business
+            moving.
+          </p>
+        </Section>
+        <Layout>
+          <Leftside />
+        </Layout>
+      </Container>
     </>
-  )
+  );
 }
-
 
 const Container = styled.div`
   padding-top: 52px;
@@ -72,7 +70,7 @@ const Layout = styled.div`
   grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
   column-gap: 25px;
   row-gap: 25px;
-  /* grid-template-row: auto; */ 
+  /* grid-template-row: auto; */
   margin: 25px 0;
   @media (max-width: 768px) {
     display: flex;
